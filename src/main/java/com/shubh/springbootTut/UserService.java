72b9fbd1f493915f6f625d9b97970ff61d71ee4a
sbisht <sbisht@opentext.com>
2026-09-19 19:54:26 +0530

    inject proeprty

--- src/main/java/com/shubh/springbootTut/UserService.java
+++ src/main/java/com/shubh/springbootTut/UserService.java
@@ -1,17 +1,20 @@
 package com.shubh.springbootTut;
 
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     DataBase dataBase;
+
+    @Value("${db.type}")
+    String property;
     public UserService(DataBase dataBase) {
         this.dataBase = dataBase;
     }
     public void saveUser(String user){
-        System.out.println("User " + user + " saved successfully.");
+        System.out.println("User " + user + " saved successfully in " + property + " database.");
         dataBase.saveUser(user);
     }
 }
